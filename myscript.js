80de8128c50046a829a3de27cbde18ad9915fa39
Carla Pereuilh Crocitto <carlap17@gmail.com>
2023-10-04 22:34:24 -0300

    json storage y eventos

--- myscript.js
+++ myscript.js
@@ -1,171 +1,174 @@
 let productos = [
-    { nombre: "Esmalte Semipermanente Meliné", tipo: "Semipermanente", precio: 4900, stock: 3, id: "1" },
-    { nombre: "Esmalte Semipermanente Kiki", tipo: "Semipermanente", precio: 2400, stock: 1, id: "2" },
-    { nombre: "Esmalte Semipermanente Juka", tipo: "Semipermanente", precio: 3100, stock: 6, id: "3" },
-    { nombre: "Esmalte Tradicional OPI", tipo: "Tradicional", precio: 4800, stock: 5, id: "4" },
-    { nombre: "Esmalte Tradicional Essie", tipo: "Tradicional", precio: 11700, stock: 5, id: "5" },
-    { nombre: "Esmalte Tradicional Gelize", tipo: "Tradicional", precio: 19900, stock: 9, id: "6" },
-    { nombre: "Press On punta Coffin x600u", tipo: "Accesorios", precio: 2200, stock: 6, id: "7" },
-    { nombre: "Polvo Aurora", tipo: "Accesorios", precio: 1500, stock: 4, id: "8" },
-    { nombre: "Polvo Holográfico", tipo: "Accesorios", precio: 1430, stock: 4, id: "9" },
-    { nombre: "Cabina Gadnic Led UV", tipo: "Cabina", precio: 33099, stock: 3, id: "10" },
-    { nombre: "Cabina Sun Led UV", tipo: "Cabina", precio: 13999.99, stock: 7, id: "11" }
+    { nombre: "Esmalte Semipermanente Meliné", tipo: "Semipermanente", precio: 4900, stock: 3, id: "1", rutaImagen: "semipermanente1.jpeg" },
+    { nombre: "Esmalte Semipermanente Kiki", tipo: "Semipermanente", precio: 2400, stock: 1, id: "2", rutaImagen: "semipermanente2.jpeg" },
+    { nombre: "Esmalte Semipermanente Juka", tipo: "Semipermanente", precio: 3100, stock: 6, id: "3", rutaImagen: "semipermanente3.jpeg" },
+    { nombre: "Esmalte Tradicional OPI", tipo: "Tradicional", precio: 4800, stock: 5, id: "4", rutaImagen: "tradicional1.jpg" },
+    { nombre: "Esmalte Tradicional Essie", tipo: "Tradicional", precio: 11700, stock: 5, id: "5", rutaImagen: "tradicional2.jpg" },
+    { nombre: "Esmalte Tradicional Gelize", tipo: "Tradicional", precio: 19900, stock: 9, id: "6", rutaImagen: "tradicional3.jpg" },
+    { nombre: "Press On punta Coffin x600u", tipo: "Accesorios", precio: 2200, stock: 6, id: "7", rutaImagen: "press-on.jpg" },
+    { nombre: "Polvo Aurora", tipo: "Accesorios", precio: 1500, stock: 4, id: "8", rutaImagen: "brillos1.jpg" },
+    { nombre: "Polvo Holográfico", tipo: "Accesorios", precio: 1430, stock: 4, id: "9", rutaImagen: "brillos2.jpg" },
+    { nombre: "Cabina Gadnic Led UV", tipo: "Cabina", precio: 33099, stock: 3, id: "10", rutaImagen: "cabina1.jpg" },
+    { nombre: "Cabina Sun Led UV", tipo: "Cabina", precio: 13999.99, stock: 7, id: "11", rutaImagen: "cabina2.jpg" }
 ];
 
+// Copia de respaldo de la lista de productos original
+let productosOriginales = [...productos];
 
-let carrito = [];
+let carritoRecuperado = localStorage.getItem("carrito");
+let carrito = carritoRecuperado ? JSON.parse(carritoRecuperado) : [];
 
-function principal(productos) {
+// Función para renderizar productos
+function renderizarProductos(productos, carrito) {
+    let contenedor = document.getElementById("contenedorProd");
+    contenedor.innerHTML = "";
 
-    while (true) {
-        let valor = Number(prompt(`Bienvenida/o a la tienda de Nail Art!\n\nIngrese la opción que desea aplicar:\n1- Ver lista de productos\n2- Agregar productos al carrito\n3- Finalizar compra\n4- Filtrar productos por categoría\n5- Ordenar productos de forma ascendente\n6- Ordenar productos de forma descendente\n7- Ordenar productos por precio ascendente\n8- Ordenar productos por precio descendente\n0- Salir`));
+    productos.forEach(producto => {
+        let tarjeta = document.createElement("div");
+        tarjeta.className = "tarjeta";
+        tarjeta.innerHTML = `<img src=./images/${producto.rutaImagen}>
+        <h4>${producto.nombre}</h4>
+        <p>$${producto.precio}</p>
+        <button id=${producto.id}>Agregar al carrito</button>
+        `;
+        contenedor.appendChild(tarjeta);
 
-        if (valor === 0) {
-            break
+        let botonAgregarAlCarrito = document.getElementById(producto.id);
+        botonAgregarAlCarrito.addEventListener("click", (e) => agregarProductoAlCarrito(producto, carrito));
+        
+        // Verificar si el producto está en el carrito
+        if (carrito.find(item => item.id === producto.id)) {
+            botonAgregarAlCarrito.disabled = true;
         }
-
-        switch (valor) {
-            case 1:
-                alert(listar(productos))
-                break
-            case 2:
-                while (true) {
-                    const idCompra = prompt(`Ingrese el ID del producto que desea agregar al carrito:\n${listar(productos)}`);
-                    
-                    if (idCompra === "0") {
-                        break;
-                    }
-                    
-                    agregarAlCarrito(idCompra);
-
-                    let continuarComprando = Number(prompt("¿Qué desea hacer?\n1- Seguir comprando\n2- Finalizar compra\n0- Volver al menú principal"));
-                    
-                    if (continuarComprando === 2) {
-                        finalizarCompra(carrito);
-                        return;
-                    } else if (continuarComprando === 0) {
-                        break;
-                    }
-                }
-                break;            
-            case 3:
-                finalizarCompra(carrito)
-                break
-            case 4:
-                const tipo = prompt(`Ingrese número de la categoría que desea filtrar:\n1- Semipermanente\n2- Tradicional\n3- Accesorios\n4- Cabinas`);
-    
-                if (tipo === "1" || tipo === "2" || tipo === "3" || tipo === "4") {
-                    const tipoFiltrado = obtenerNombreCategoria(Number(tipo));
-                    const productosFiltrados = filtrarPorTipo(productos, tipoFiltrado);
-
-                    if (productosFiltrados.length === 0) {
-                        alert("No se encontraron productos en esta categoría.");
-                    } else {
-                        alert(listar(productosFiltrados));
-                    }
-                } else {
-                    alert("Número de categoría no válido.");
-                }
-                break;
-            case 5:
-                ordenarProductos(true)
-                alert("Productos ordenados por precio ascendente a continuación:\n\n" + listar(productos))
-                break
-            case 6:
-                ordenarProductos(false)
-                alert("Productos ordenados por precio descendente a continuación:\n\n" + listar(productos))
-                break
-            case 7:
-                ordenarAlfabetico(true)
-                alert("Productos ordenados alfabeticamente de forma ascendente a continuación:\n\n" + listar(productos))
-                break
-            case 8:
-                ordenarAlfabetico(false)
-                alert("Productos ordenados alfabeticamente de forma descendente a continuación:\n\n" + listar(productos))
-                break
-            default:
-                alert("Opción no válida.")
-                break
-        }
-    }
+    });
 }
 
-function agregarAlCarrito(id) {
-    let producto = productos.find(producto => producto.id === id);
-    if (producto && producto.stock > 0) {
-        let cantidad = Number(prompt(`Ingrese la cantidad de "${producto.nombre}" que desea agregar al carrito:\n(Stock: "${producto.stock}")`));
+// Función para agregar un producto al carrito
+function agregarProductoAlCarrito(producto, carrito) {
+    let productoEnCarrito = carrito.find(item => item.id === producto.id);
 
-        if (cantidad > 0 && cantidad <= producto.stock) {
-            let productoEnCarrito = carrito.find(item => item.id === id);
-
-            if (productoEnCarrito) {
-                productoEnCarrito.cantidad += cantidad;
-            } else {
-                carrito.push({ ...producto, cantidad });
-            }
-
-            producto.stock -= cantidad;
-            alert(`Se agregaron ${cantidad} "${producto.nombre}" al carrito.`);
+    if (producto.stock > 0) {
+        if (productoEnCarrito) {
+            productoEnCarrito.cantidad++;
         } else {
-            alert("Cantidad no válida o no hay suficiente stock.");
+            carrito.push({
+                id: producto.id,
+                nombre: producto.nombre,
+                precio: producto.precio,
+                cantidad: 1
+            });
         }
+        producto.stock--;
+        localStorage.setItem("carrito", JSON.stringify(carrito));
     } else {
-        alert("Producto no disponible o sin stock.");
+        alert("No hay más stock del producto seleccionado");
+    }
+
+    renderizarCarrito(carrito);
+}
+
+// Función para renderizar el carrito
+function renderizarCarrito(carrito) {
+    let divCarrito = document.getElementById("carrito");
+    divCarrito.innerHTML = "";
+
+    let precioTotal = 0
+
+    carrito.forEach(item => {
+        let tarjetaCarrito = document.createElement("div");
+        tarjetaCarrito.innerHTML = `
+        <p>${item.nombre}</p>
+        <p>Precio: $${item.precio}</p>
+        <p>Cantidad: ${item.cantidad}</p>
+        <p>Subtotal: $${item.precio * item.cantidad}</p>
+        `;
+        divCarrito.appendChild(tarjetaCarrito);
+        
+        precioTotal += item.precio * item.cantidad
+    });
+
+    if (precioTotal != 0) {
+        let totalLine = document.createElement("div")
+        totalLine.innerHTML = `<p>Total a pagar: $${precioTotal}</p>`
+        divCarrito.appendChild(totalLine)
+    
+        let boton = document.createElement("button");
+    boton.innerHTML = "Finalizar compra";
+    boton.addEventListener("click", finalizarCompra);
+    divCarrito.appendChild(boton);
     }
 }
 
-function finalizarCompra(carrito) {
-    if (carrito.length === 0) {
-        alert("El carrito está vacío. No se puede finalizar la compra.");
-    } else {
-        let total = 0;
-        carrito.forEach(producto => {
-            total += producto.precio * producto.cantidad;
-        });
-        alert(`Total de la compra: $${total}`);
-        alert(`Gracias por su compra. ¡Vuelva pronto!`);
-        carrito.length = 0; 
+// Función para finalizar la compra
+function finalizarCompra() {
+    localStorage.removeItem("carrito");
+    carrito = [];
+    renderizarCarrito(carrito);
+
+    alert("¡Gracias por su compra!")
+}
+
+// Función para filtrar y renderizar productos
+function filtrarYRenderizar() {
+    let buscador = document.getElementById("buscador").value.toLowerCase();
+    let productosFiltrados = productos.filter(producto => producto.nombre.toLowerCase().includes(buscador));
+    renderizarProductos(productosFiltrados, carrito);
+}
+
+// Evento para el botón de buscar
+let buscar = document.getElementById("buscar");
+buscar.addEventListener("click", filtrarYRenderizar);
+
+let buscador = document.getElementById("buscador");
+buscador.addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        // Filtrar productos cuando se presiona Enter
+        filtrarYRenderizar();
     }
+});
+
+// Llamada inicial para renderizar productos
+renderizarProductos(productos, carrito);
+
+let botonVerOcultar = document.getElementById("verOcultar");
+botonVerOcultar.addEventListener("click", verOcultarCarrito);
+
+function verOcultarCarrito() {
+    let carrito = document.getElementById("carrito");
+    let contenedorProd = document.getElementById("contenedorProd");
+    carrito.classList.toggle("oculta");
+    contenedorProd.classList.toggle("oculta");
 }
 
-function listar(productos) {
-    return productos.map(producto => `ID: ${producto.id} - ${producto.nombre}: $${producto.precio}`).join("\n")
-}
+let resetearBusqueda = document.getElementById("resetearBusqueda");
+resetearBusqueda.addEventListener("click", reiniciarListaProductos);
 
-function listarCarrito(carrito) {
-    let lista = "Carrito de compras:\n\n"
-    carrito.forEach(producto => {
-        lista += `${producto.nombre} x${producto.cantidad}: $${(producto.precio * producto.cantidad).toFixed(2)}\n`;
-    })
-    return lista
-}
+// Obtener el elemento de selección de categoría
+let filtroCategoria = document.getElementById("filtroCategoria");
 
-function ordenarProductos(ascendente) {
-    productos.sort((a, b) => (ascendente ? a.precio - b.precio : b.precio - a.precio))
-}
+// Agregar un evento que se dispare cuando cambie la selección
+filtroCategoria.addEventListener("change", filtrarPorCategoria);
 
-function ordenarAlfabetico(ascendente) {
-    productos.sort((a, b) => (ascendente ? a.nombre.localeCompare(b.nombre) : b.nombre.localeCompare(a.nombre)))
-}
+function filtrarPorCategoria() {
+    // Obtener la categoría seleccionada
+    let categoriaSeleccionada = filtroCategoria.value;
 
-function filtrarPorTipo(productos, tipo) {
-    return productos.filter(producto => producto.tipo === tipo);
-}
+    // Filtrar los productos según la categoría
+    let productosFiltrados = productos;
 
-function obtenerNombreCategoria(numero) {
-    switch (numero) {
-        case 1:
-            return "Semipermanente";
-        case 2:
-            return "Tradicional";
-        case 3:
-            return "Accesorios";
-        case 4:
-            return "Cabina";
-        default:
-            return null;
+    if (categoriaSeleccionada !== "") {
+        productosFiltrados = productos.filter(producto => producto.tipo === categoriaSeleccionada);
     }
+
+    // Renderizar los productos filtrados
+    renderizarProductos(productosFiltrados, carrito);
 }
 
-
-principal(productos);
+// Función para reiniciar la lista de productos
+function reiniciarListaProductos() {
+    // Utiliza la lista de productos original
+    renderizarProductos(productosOriginales, carrito);
+    document.getElementById("buscador").value = ""; // Limpia el campo de búsqueda
+}
+// Llamada inicial para renderizar el carrito
+renderizarCarrito(carrito);
